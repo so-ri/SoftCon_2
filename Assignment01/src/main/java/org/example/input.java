d295cc526fd657d0587ec913f6e1537fc93401d1
Elyisha <116687078+Elyisha@users.noreply.github.com>
2022-10-29 14:12:12 +0200

    Update input.java

--- Assignment01/src/main/java/org/example/input.java
+++ Assignment01/src/main/java/org/example/input.java
@@ -1,6 +1,17 @@
 package org.example;
-
+import java.util.Scanner;
+import java.util.Random;
+//import java.util.Arrays;
 public class input {
+    public static void ScanShips(){
+        Scanner ShipInput = new Scanner(System.in);
+        String Answer = null;
+        String Ships[] = {"Battleship", "Carrier", "Cruiser", "Destroyer", "Submarine"};
+
+
+
+
+    }
 
     public static void placeComputerShips(board computerBoard){
         //place computers ships
@@ -9,6 +20,16 @@ public class input {
     public static void placePlayerShips(board playerBoard){
         //place player ship
     }
+    public static void createShip(){
+
+
+
+    }
+    public static void isValidShip(){
+
+
+    }
+
 
 
 }
