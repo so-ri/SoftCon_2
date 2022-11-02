48c93d668212d60fd4fcc7bf332e680581701d08
Elyisha <116687078+Elyisha@users.noreply.github.com>
2022-11-02 10:14:20 +0100

    up

--- Assignment01/src/main/java/org/example/Input.java
+++ Assignment01/src/main/java/org/example/Input.java
@@ -5,11 +5,12 @@ import org.example.ships.*;
 
 
 public class Input {
+    static boolean ComputerIsScanning = false;
     static final String Ships[] = {"Carrier", "first Battleship", "second Battleship",
             "first Submarine", "second Submarine","third Submarine", "first Patrolboat",
             "second Patrolboat", "third Patrolboat", "fourth Patrolboat"};
     public static void ScanPlayerShips(board Playerboard){
-/*
+        /*
         Playerboard.createShip(positionX.A, positionY.ZERO, positionX.A, positionY.FIVE);
         Playerboard.createShip(positionX.B, positionY.ZERO, positionX.B, positionY.THREE);
         Playerboard.createShip(positionX.B, positionY.FIVE, positionX.B, positionY.EIGHT);
@@ -31,8 +32,6 @@ public class Input {
                 String pos1 = scan.substring(0,2);
                 String pos2 = scan.substring(3);
 
-
-
                 if(isValidShip(pos1,pos2, Ships[i], Playerboard)) {
                     createShip(pos1,pos2, Ships[i], Playerboard);
                     e = false;
@@ -43,8 +42,10 @@ public class Input {
             }
         }
     }
+
     public static void ScanComputerShips(board Computerboard) {
         Random rnd = new Random();
+        ComputerIsScanning = true;
         int len;
         char F1;
         char F2;
@@ -56,73 +57,33 @@ public class Input {
                 char P2 = (char) ('0' + rnd.nextInt(10));
                 String pos1 = P1 + Character.toString(P2);
 
-                int dir = rnd.nextInt(4);
-                switch(dir) {
-                    case 0:
+                int dir = rnd.nextInt(2);
+                    if(dir == 0) {
                         len = isRightShip(Ships[i]);
-                        F1 = (char) ((int) P1 - len-1);
+                        F1 = (char) ((int) P1 + len - 1);
                         pos2 = F1 + Character.toString(P2);
-                        if(!onBoard(pos2)) {
-                            continue;
-                        }
-                        else if(isValidShip(pos1, pos2, Ships[i], Computerboard)) {
+                        if (onBoard(pos2) && isValidShip(pos1, pos2, Ships[i], Computerboard)) {
                             createShip(pos1, pos2, Ships[i], Computerboard);
                             e = false;
                             break;
-                        }
-                        else {
-                            continue;
-                        }
-                    case 1:
-                        len = isRightShip(Ships[i]);
-                        F1 = (char) ((int) P1 + len-1);
-                        pos2 = F1 + Character.toString(P2);
-                        if(!onBoard(pos2)) {
-                            continue;
-                        }
-                        else if(isValidShip(pos1, pos2, Ships[i], Computerboard)) {
-                            createShip(pos1, pos2, Ships[i], Computerboard);
-                            e = false;
-                            break;
-                        }
-                        else {
-                            continue;
                         }
 
-                    case 2:
-                        len = isRightShip(Ships[i]);
-                        F2 = (char) ((int) P2 + len-1);
-                        pos2 = P1 + Character.toString(F2);
-                        if(!onBoard(pos2)) {
-                            continue;
-                        }
-                        else if(isValidShip(pos1, pos2, Ships[i], Computerboard)) {
-                            createShip(pos1, pos2, Ships[i], Computerboard);
-                            e = false;
-                            break;
-                        }
-                        else {
-                            continue;
-                        }
 
-                    case 3:
+                    }
+                    else {
                         len = isRightShip(Ships[i]);
-                        F2 = (char) ((int) P2 - len-1);
+                        F2 = (char) ((int) P2 + len - 1);
                         pos2 = P1 + Character.toString(F2);
-                        if(!onBoard(pos2)) {
-                            continue;
-                        }
-                        else if(isValidShip(pos1, pos2, Ships[i], Computerboard)) {
+
+                        if (onBoard(pos2) && isValidShip(pos1, pos2, Ships[i], Computerboard)) {
                             createShip(pos1, pos2, Ships[i], Computerboard);
                             e = false;
                             break;
                         }
-                        else {
-                            continue;
-                        }
-                }
+                    }
             }
         }
+        ComputerIsScanning = false;
     }
 
 
@@ -153,10 +114,10 @@ public class Input {
         }
         //NOT WHEN COMPUTER DOES IT
         if(getlen(Spos,Epos) != isRightShip(typus)) {
-            System.out.println("Ship doesnt have proper length");
-
+            if(!ComputerIsScanning) System.out.println("Ship doesnt have proper length");
             return false;
         }
+
         positionX x1 = Guess.translateX(pos1);
         positionY y1 = Guess.translateY(pos1);
         positionX x2 = Guess.translateX(pos2);
@@ -166,44 +127,13 @@ public class Input {
         positionX[] xes = test.getXcoordinates();
         positionY[] yes = test.getYcoordinates();
         for(int i = 0; i < xes.length; ++i) {
-            // i not 0
             if(!board2.IsEmpty(xes[i], yes[i])) {
                 return false;
-
             }
         }
         return true;
-
     }
-    /*
-    public static String Shipscan(String msg, board Board1) {
-        Scanner Input = new Scanner(System.in);
-        String answer = null;
-        boolean i = true;
-        //Checks if valid input according to PRE-condition
-        System.out.println(msg);
-        while(i) {
-            String pos2 = Input.nextLine();
-            String pos1 = pos2.replaceAll("\\s+","");
-            String pos = pos1.toUpperCase();
-            if(pos.length()!=2) {
-                System.out.println("Please type in a Position on the Board");
-            }
-            else if((int) pos.charAt(0) < 65 || (int) pos.charAt(0) > 74
-                    || (int) pos.charAt(1) < 48 || (int) pos.charAt(1) > 57) {
-                System.out.println("Please type in a Position on the Board");
-            }
 
-            else if(!Guess.ValidShot(pos, Board1)) {
-                System.out.println("Please type in a valid Position");
-            }
-            else {
-                i = false;
-                answer = pos;
-            }
-        }
-        return answer;
-    }*/
 
     public static String Shipscan(String msg, board Board1) {
         Scanner Input = new Scanner(System.in);
